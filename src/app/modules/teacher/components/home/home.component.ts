dd38f65c056323c5aa8f7d048f69f3acb1b0fb33
Zunoor Ahmad <zunoor19@gmail.com>
2025-04-21 00:04:12 +0500

    routing issue resolved

--- src/app/modules/teacher/components/home/home.component.ts
+++ src/app/modules/teacher/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(public global: GlobalService) { }
 
 }
